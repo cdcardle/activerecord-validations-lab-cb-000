561e232bdc18979d71ad91f165991c025b27a3c7
Chris Cardle <cdcardle@gmail.com>
2018-05-31 14:40:14 +0000

    automatically backed up by learn

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -2,7 +2,7 @@ class ClickbaitValidator < ActiveModel::EachValidator
   # clickbait_regex = /\b(Won't Believe|Guess|Secret|Top \d*)\b/
 
   def validate_each(record, attribute, value)
-    if value.empty?
+    if value.nil?
       record.errors.add(attribute, 'name cannot be blank')
     elsif value.match(/\b(Won't Believe|Guess|Secret|Top \d*)\b/).blank?
       record.errors.add(attribute, 'not clickbait-y enough')
