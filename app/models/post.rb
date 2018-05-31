7670c5639343c22358fc1f4bd540068e6ca078e0
Chris Cardle <cdcardle@gmail.com>
2018-05-31 14:38:06 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: :allow_nil => true, true, clickbait: true
+  validates :title, presence: true, :allow_nil => true, clickbait: true
   validates :content, length: {minimum: 50}
   validates :summary, length: {maximum: 50}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "%{value} is not Fiction or Non-Fiction."}
